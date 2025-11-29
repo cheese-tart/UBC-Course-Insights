bf94513a0b7bb82192cd1294cbe1c9c0a7e27bfa
Chester <hzx@students.cs.ubc.ca>
2025-11-28 16:19:42 -0800

    Implemented App.ts in src

--- src/App.ts
+++ src/App.ts
@@ -1,1 +1,21 @@
+import { Log } from "@ubccpsc310/project-support";
+import Server from "./rest/Server";
 
+export class App {
+	public async initServer(port: number): Promise<void> {
+		Log.info();
+		const server = new Server(port);
+		return server.start().then(() => {
+			Log.info();
+		}).catch((err: Error) => {
+			Log.error(err);
+		});
+	}
+}
+
+Log.info("App - starting");
+const app = new App();
+(async (): Promise<void> => {
+	const port = 67;
+	await app.initServer(port);
+})();
